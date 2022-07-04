9729a0d26e81c6c99d3865fff5b1a3f87f3fb42e
nataliakoliveira <nataliaakellyy@gmail.com>
2022-07-04 15:28:44 -0300

    requisito2 e requisito3

---
+++ script.js
@@ -0,0 +1,9 @@
+const paleta1 = document.getElementById('color-palette');
+paleta1.style.backgroundColor = 'black';
+const paleta2 = document.querySelectorAll('div#color-palette.color')[1];
+paleta2.style.backgroundColor = 'yellow';
+const paleta3 = document.querySelectorAll('div#color-palette.color')[2];
+paleta3.style.backgroundColor = "#FF00FF";
+const paleta4 = document.querySelectorAll('div#color-palette.color')[3];
+paleta4.style.backgroundColor = 'green';
+
